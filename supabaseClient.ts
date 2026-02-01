bbfa7052f821362c610bf5cb066cb2033f5e848c
glauprofisio-bit <glauprofisio@gmail.com>
2026-02-01 12:58:36 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zcrjsvgjnbzawrnajgva.supabase.co';
-const supabaseKey = 'sb_publishable_t01dpjzy6r1Qdag45eAMvQ_dJtOBG23';
+const supabaseUrl = 'https://bumcjbjnkblzvrjpvafn.supabase.co'; 
+const supabaseAnonKey = 'sb_publishable_8jjRyS4uqL9yLU6JdpHx9A_l-UgLSYW'; 
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
