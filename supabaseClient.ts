5b9625da3d57264d2955eac05a7570760c4358de
glauprofisio-bit <glauprofisio@gmail.com>
2026-02-01 12:05:29 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,16 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-
-
 const supabaseUrl = 'https://zcrjsvgjnbzawrnajgva.supabase.co';
-
 const supabaseKey = 'sb_publishable_t01dpjzy6r1Qdag45eAMvQ_dJtOBG23';
 
-
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
-    console.log('Database initialized successfully');
-  } catch (error) {
-    console.error('Error initializing database:', error);
-  }
-};
